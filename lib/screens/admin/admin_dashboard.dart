0d0664b529643e1d0e32f2035dd13a44447fa58e
Ismail Hossain <ismailraj127413@gmail.com>
2025-09-08 23:46:09 +0600

    9th commit

--- lib/screens/admin/admin_dashboard.dart
+++ lib/screens/admin/admin_dashboard.dart
@@ -5,6 +5,7 @@ import 'package:provider/provider.dart';
 import '../../providers/auth_provider.dart';
 import '../../routes/app_routes.dart';
 import 'student_management.dart';
+// Ensure that 'student_management.dart' exports a class named 'StudentManagementScreen'.
 import 'payment_management.dart';
 
 class AdminDashboard extends StatelessWidget {
--- lib/services/admin_data_service.dart
+++ lib/services/admin_data_service.dart
@@ -1,6 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AdminDataService {
+  Future<List<Map<String, dynamic>>> getAllStudents() async {
+    final query = await _firestore.collection('Students').get();
+    return query.docs.map((doc) {
+      final data = doc.data();
+      return {
+        'name': data['name'] ?? '',
+        'email': data['email'] ?? '',
+        'status': data['status'] ?? 'Active',
+        'avatar': data['avatar'] ?? 'https://i.pravatar.cc/150?u=${doc.id}',
+      };
+    }).toList();
+  }
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   Future<int> getStudentCount() async {
